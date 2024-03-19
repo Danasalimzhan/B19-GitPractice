11b12f1ae631025e3dbe90683be67c7e6befbabb
Dana <salimzhandauren@gmail.com>
2024-03-19 14:34:44 -0500

    for loop added

--- src/test/java/git_intro/FirstGit.java
+++ src/test/java/git_intro/FirstGit.java
@@ -5,5 +5,9 @@ public class FirstGit {
         System.out.println("My first git practice Dana");
         System.out.println("my second git push Dana");
         System.out.println("my third git push Dana");
+        for(int i=0; i<5; i++){
+            System.out.println(i);
+            System.out.println("added another line of code branch Dana");
+        }
     }
 }
