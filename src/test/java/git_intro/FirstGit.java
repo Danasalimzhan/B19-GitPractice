3034856e9051a9baeb80d4cf348317852747ddba
Dana <salimzhandauren@gmail.com>
2024-03-19 14:13:14 -0500

    my second push

--- src/test/java/git_intro/FirstGit.java
+++ src/test/java/git_intro/FirstGit.java
@@ -2,6 +2,7 @@ package git_intro;
 
 public class FirstGit {
     public static void main(String[] args) {
-        System.out.println("Dana");
+        System.out.println("My first git practice Dana");
+        System.out.println("my second git push Dana");
     }
 }
